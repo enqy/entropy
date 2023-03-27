a822278a1fad936de2da3258c942120c8b838784
Woze Parrot <wozeparrot@gmail.com>
2023-03-27 02:03:10 -0400

    flake: redisable darwin buildSystem

--- flake.nix
+++ flake.nix
@@ -76,8 +76,8 @@
     eachSystem [
       "x86_64-linux"
       "aarch64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
+      # "x86_64-darwin"
+      # "aarch64-darwin" # still broken
     ]
     (
       system: let
